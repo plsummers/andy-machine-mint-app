7ba36ec75956f0959fdf08896875c9a24d846396
RuSS512 <vip.rusel2@gmail.com>
2022-03-11 00:17:59 +0300

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -28,7 +28,16 @@ import {
   getAtaForMint,
 } from "./utils";
 
-const ConnectButton = styled(WalletDialogButton)``;
+const ConnectButton = styled(WalletDialogButton)`
+  width: 100%;
+  height: 60px;
+  margin-top: 10px;
+  margin-bottom: 5px;
+  background: linear-gradient(180deg, #604ae5 0%, #813eee 100%);
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+  `;
 
 const MintContainer = styled.div``; // add your styles here
 
