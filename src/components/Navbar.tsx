5df4dcb4caa3be89489380b24ed8da1b76f3fdbe
RuSS512 <vip.rusel2@gmail.com>
2022-03-12 17:29:12 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Container1 = styled("div")`
     width: 100vw;
-    height: 100px;
+    height: 30px;
     background-color: #ffffff;
 
     display: flex;
