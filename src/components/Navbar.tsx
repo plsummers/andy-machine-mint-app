04b3fb3ac109250346849d2ec8a0e207a3d4599f
RuSS512 <vip.rusel2@gmail.com>
2022-03-12 16:57:46 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,41 @@
+import styled from "styled-components";
 
+const Container = styled("div")`
+    width: 100vw;
+    height: auto;
+    background-color: #af48ca;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+`
+
+const Title = styled("h1")`
+    margin: 0;
+    white-space: nowrap;
+`
+
+const Link = styled("a")`
+    font-size: 1.5rem;
+    margin: 1rem;
+    cursor: pointer;
+    color: initial;
+    text-decoration: none;
+    }
+`
+
+const Navbar = () => {
+    return (
+        <Container>
+            <Title>
+              <Link href="https://soletters.io/">
+                Titan Dogs
+                </Link>
+            </Title>
+        </Container>
+    )
+}
+
+export default Navbar;
